b853e9d30587feccf1ae99a83ebea60f96c1877a
unknown <adriano.contacto@gmail.com>
2021-03-11 12:47:20 -0300

    Agrega logica para comparar cartas
    Compara cartas pero al presionar una carta el evento 'click' se ejecuta doblemente
    por lo que el juego no funciona de manera optima

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,16 @@
 // Crear x cantidad de divs para cada una de las imagenes
 //
+/* 
+  determinar que la cantidad de cartas a comparar son 2
+  al hacer click en una carta guardar su valor en un Array
+  y lo mismo con la siguiente. 
+  Al array ser igual a 2 se determina que el array esta completo y se comparan sus valores
+  si sus valores son iguales se eliminan ambas cartas y se suma 1 punto
+  al llegar a 6 puntos termina el juego
+  */
+
+const $playBtn = document.querySelector("#start-game");
+const cardsFront = document.querySelectorAll(".card-front");
 const cardsBody = document.querySelectorAll(".card-body");
 const cardsBack = document.querySelectorAll(".card-back");
 const image1 = "img/angular.png";
@@ -9,37 +20,91 @@ const image4 = "img/JS.png";
 const image5 = "img/node.png";
 const image6 = "img/react.png";
 
-const images = [image1,image2,image3,image4,image5,image6];
+const images = [image1, image2, image3, image4, image5, image6];
+let comparedCards = []
+let comparedImages = []
 
-// let arr = [1, 2, 3, 4, 5, 6, 7, 8];
-// function reorderArray(a) {
-//   a.sort(function () {
-//     return Math.random() - 0.5;
-//   });
-//   console.log(a);
-// }
-// console.log(reorderArray(arr))
 
-// reorderArray(arr);
+$playBtn.onclick = function () {
+  const newArray = reorderArray(images);
+  imgAssignement(newArray);
+  roundHandler();
+  
+};
 
-  function flip() {
-    cardsBody.forEach(card => {
-      card.addEventListener('click', function () {
-        card.classList.toggle('flipped')
-      })
-    });
-  }
+function roundHandler() {
+  comparedCards = []
+  comparedImages = []
+  enableFlip();
+  catchCards();
+}
 
-function imgAssignement(){
-  let counter = 0
-images.forEach(img => {
-  console.log(img)
-  let cardBack = cardsBack[counter]
-  cardBack.style.cssText+=`background-image:url(${img})`
-  counter++
-  console.log(counter)
+function reorderArray(a) {
+  duplicateArrayValues(images);
+  const newArray = a.sort(function () {
+    return Math.random() - 0.5;
+  });
+  return newArray;
+}
+
+function duplicateArrayValues(arr) {
+  arr.forEach((i, a, c) => {
+    images.push(i);
   });
 }
 
-imgAssignement()
-flip()
+function enableFlip() {
+  cardsBody.forEach((card) => {
+    card.addEventListener("click", function () {
+      card.classList.toggle("flipped");
+    });
+  });
+}
+
+function imgAssignement(newArray) {
+  let counter = 0;
+  newArray.forEach((img) => {
+    let cardBack = cardsBack[counter];
+    cardBack.style.cssText += `background-image:url(${img})`;
+    counter++;
+  });
+}
+
+
+function compareCards(arr) {
+  for (let i = 0; i < 1; i++) {
+    const firstCard = arr[i];
+    const secondCard = arr[i + 1];
+    console.log(firstCard === secondCard);
+    if (firstCard === secondCard) {
+      deleteCards(comparedCards);
+    }
+  }
+  roundHandler ()
+}
+
+
+function catchCards() {
+  cardsBody.forEach((card, i) => {
+    card.addEventListener("click", function () {
+      comparedCards.push(card);
+      comparedImages.push(cardsBack[i].style.backgroundImage);
+      console.log(comparedImages);
+      if (comparedImages.length === 2) {
+        setTimeout(() => {
+          compareCards(comparedImages);
+        }, 500);
+      }
+    });
+  });
+}
+
+
+
+function deleteCards(cards) {
+  cards.forEach((card) => {
+    card.style.visibility = "hidden";
+  });
+  comparedCards = []
+  comparedImages = []
+}
